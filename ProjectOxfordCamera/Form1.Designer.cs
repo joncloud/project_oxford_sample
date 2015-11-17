5b4a09b702df363d2a655a2d21add142da6e6f15
joncloud <jberube@accela.com>
2015-11-17 13:22:49 -0800

    Initial
    Former-commit-id: dcd3036378caf9c7ee8d3fcc47f146c7f302e324

---
+++ ProjectOxfordCamera/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ProjectOxfordCamera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxPicture = new System.Windows.Forms.GroupBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.labelMode = new System.Windows.Forms.Label();
+            this.comboBoxMode = new System.Windows.Forms.ComboBox();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonAnalyze = new System.Windows.Forms.Button();
+            this.groupBoxPicture.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxPicture
+            // 
+            this.groupBoxPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxPicture.Controls.Add(this.pictureBox);
+            this.groupBoxPicture.Location = new System.Drawing.Point(12, 65);
+            this.groupBoxPicture.Name = "groupBoxPicture";
+            this.groupBoxPicture.Size = new System.Drawing.Size(397, 216);
+            this.groupBoxPicture.TabIndex = 0;
+            this.groupBoxPicture.TabStop = false;
+            this.groupBoxPicture.Text = "Picture";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(3, 16);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(391, 197);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // labelMode
+            // 
+            this.labelMode.AutoSize = true;
+            this.labelMode.Location = new System.Drawing.Point(12, 15);
+            this.labelMode.Name = "labelMode";
+            this.labelMode.Size = new System.Drawing.Size(37, 13);
+            this.labelMode.TabIndex = 1;
+            this.labelMode.Text = "Mode:";
+            // 
+            // comboBoxMode
+            // 
+            this.comboBoxMode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMode.FormattingEnabled = true;
+            this.comboBoxMode.Items.AddRange(new object[] {
+            "Camera",
+            "File"});
+            this.comboBoxMode.Location = new System.Drawing.Point(85, 12);
+            this.comboBoxMode.Name = "comboBoxMode";
+            this.comboBoxMode.Size = new System.Drawing.Size(324, 21);
+            this.comboBoxMode.TabIndex = 2;
+            this.comboBoxMode.SelectedIndexChanged += new System.EventHandler(this.comboBoxMode_SelectedIndexChanged);
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(12, 42);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(32, 13);
+            this.labelPath.TabIndex = 3;
+            this.labelPath.Text = "Path:";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPath.Location = new System.Drawing.Point(85, 39);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(324, 20);
+            this.textBoxPath.TabIndex = 4;
+            // 
+            // buttonAnalyze
+            // 
+            this.buttonAnalyze.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonAnalyze.Location = new System.Drawing.Point(12, 287);
+            this.buttonAnalyze.Name = "buttonAnalyze";
+            this.buttonAnalyze.Size = new System.Drawing.Size(75, 23);
+            this.buttonAnalyze.TabIndex = 5;
+            this.buttonAnalyze.Text = "Analyze";
+            this.buttonAnalyze.UseVisualStyleBackColor = true;
+            this.buttonAnalyze.Click += new System.EventHandler(this.buttonAnalyze_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 322);
+            this.Controls.Add(this.buttonAnalyze);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.comboBoxMode);
+            this.Controls.Add(this.labelMode);
+            this.Controls.Add(this.groupBoxPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBoxPicture.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxPicture;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label labelMode;
+        private System.Windows.Forms.ComboBox comboBoxMode;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonAnalyze;
+    }
+}
+
