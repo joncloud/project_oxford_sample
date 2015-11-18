fd4b72eb86cf8ed701360961f52bb88e4f0674ba
joncloud <jdberube@gmail.com>
2015-11-18 06:22:36 -0800

    Moved capturing analysis logic into its own class.

--- ProjectOxfordCamera/Form1.cs
+++ ProjectOxfordCamera/Form1.cs
@@ -122,53 +122,13 @@ namespace ProjectOxfordCamera
 
             try
             {
-                using (Stream buffer = new MemoryStream())
-                {
-                    _copy.Save(buffer, ImageFormat.Jpeg);
-                    buffer.Seek(0, SeekOrigin.Begin);
+                EmotionAnalyzer analyzer = new EmotionAnalyzer();
 
-                    using (HttpClient client = new HttpClient())
-                    {
-                        client.BaseAddress = new Uri("https://api.projectoxford.ai/emotion/v1.0/");
-                        client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "");
+                IEnumerable<EmotionAnalysisResult> results = await analyzer.AnalyzeAsync(_copy);
 
-                        var requestContent = new StreamContent(buffer);
-
-                        requestContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
-                        HttpResponseMessage message = await client.PostAsync(
-                            "recognize",
-                            requestContent);
-
-                        message.EnsureSuccessStatusCode();
-
-                        string content = await message.Content.ReadAsStringAsync();
-
-                        JArray array = JsonConvert.DeserializeObject<JArray>(content);
-                        List<EmotionAnalysisResult> results = new List<EmotionAnalysisResult>();
-
-                        foreach (JObject jobject in array)
-                        {
-                            JObject faceRectangle = jobject.Value<JObject>("faceRectangle");
-                            EmotionAnalysisResult result = new EmotionAnalysisResult
-                            {
-                                Hitbox = new Rectangle(
-                                    faceRectangle.Value<int>("left"),
-                                    faceRectangle.Value<int>("top"),
-                                    faceRectangle.Value<int>("width"),
-                                    faceRectangle.Value<int>("height"))
-                            };
-                            JObject scores = jobject.Value<JObject>("scores");
-                            foreach (var property in scores.Properties())
-                            {
-                                result.Indexes.Add(property.Name, property.Value.Value<float>());
-                            }
-                            results.Add(result);
-                        }
-
-                        _results = results.ToArray();
-                        pictureBox.Invalidate();
-                    }
-                }
+                _results = results.ToArray();
+                
+                pictureBox.Invalidate();
             }
             catch (Exception ex)
             {
