7a1cec69d4fd0891a22fd108f5af9341e7f996e7
joncloud <jdberube@gmail.com>
2015-11-18 06:06:40 -0800

    Moved types out into their own files.
    Former-commit-id: 9050a30ce642410e01bdb6ab90d5eb41ddcbb989

---
+++ ProjectOxfordCamera/IImageSource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOxfordCamera
+{
+    public interface IImageSource
+    {
+        CaptureFrequency Frequency { get; }
+
+        Image Capture(string path);
+
+        void Start();
+        void Stop();
+    }
+}
---
+++ ProjectOxfordCamera/WebImageSource.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOxfordCamera
+{
+    public class WebImageSource : IImageSource
+    {
+        public CaptureFrequency Frequency { get { return CaptureFrequency.Once; } }
+
+        public Image Capture(string path)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(path);
+            using (var response = request.GetResponse())
+            using (var stream = response.GetResponseStream())
+            {
+                return Image.FromStream(stream);
+            }
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
